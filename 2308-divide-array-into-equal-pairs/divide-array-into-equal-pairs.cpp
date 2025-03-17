a4f2f9796b4c8bbbd5eb97bcedbcea864c083885
Satvik Chindaluru <117159255+satvikc15@users.noreply.github.com>
2025-03-17 20:10:38 +0530

    Time: 6 ms (24.13%) | Memory: 18.1 MB (31.79%) - LeetSync

---
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int,int> mpp;
+        for(auto it:nums){
+            mpp[it]++;
+        }
+        if(mpp.size()>nums.size()/2) return false;
+        for(auto it:mpp){
+            if(it.second%2!=0) return false;
+        }
+        return true;
+    }
+};
