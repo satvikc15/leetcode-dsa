f4cb058b7df6300c91f2c6132fa0a557762b282f
Satvik Chindaluru <117159255+satvikc15@users.noreply.github.com>
2025-04-20 16:47:45 +0530

    Time: 1 ms (36.71%) | Memory: 12.4 MB (10.59%) - LeetSync

---
+++ 797-rabbits-in-forest/rabbits-in-forest.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        unordered_map<int,int> mpp;
+        for(auto it:answers){
+            mpp[it]++;
+        }
+        int ans=0;
+        for(auto [a,b]:mpp){
+            int numOfGrps = ceil((double)b/(a+1));
+            ans+=numOfGrps*(a+1);
+        }
+        return ans;
+    }
+};
