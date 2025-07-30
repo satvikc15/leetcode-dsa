1745e50a794f8b30574706eaa910467f9f9e9338
Satvik Chindaluru <117159255+satvikc15@users.noreply.github.com>
2025-07-30 19:06:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (33.62%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,41 +1,54 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
+    void bfs(vector<vector<int>>& grid, vector<vector<bool>>& vis, int& time, int& fresh) {
+        queue<pair<int, int>> q;
         int rows = grid.size(), cols = grid[0].size();
-        queue<pair<int,int>> q;
-        int fresh = 0;
-        
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                if(grid[i][j]==2){
-                    q.push({i,j});
-                } else if(grid[i][j]==1){
-                    fresh++;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                    vis[i][j] = true;
                 }
             }
         }
-        
-        if(fresh == 0) return 0;
-        
-        int minutes = -1;
-        vector<pair<int,int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
-        
-        while(!q.empty()){
-            int size = q.size();
-            minutes++;
-            while(size--){
-                auto [x,y] = q.front(); q.pop();
-                for(auto [dx,dy] : dir){
-                    int nx = x + dx, ny = y + dy;
-                    if(nx>=0 && ny>=0 && nx<rows && ny<cols && grid[nx][ny]==1){
-                        grid[nx][ny] = 2;
+
+        vector<pair<int, int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        while (!q.empty()) {
+            int sz = q.size();
+            bool rotted = false;
+            for (int i = 0; i < sz; i++) {
+                auto [a, b] = q.front();
+                q.pop();
+                for (auto [k, l] : dir) {
+                    int ni = a + k;
+                    int nj = b + l;
+                    if (ni >= 0 && nj >= 0 && ni < rows && nj < cols &&
+                        grid[ni][nj] == 1 && !vis[ni][nj]) {
+                        q.push({ni, nj});
+                        vis[ni][nj] = true;
                         fresh--;
-                        q.push({nx,ny});
+                        rotted = true;
                     }
                 }
             }
+            if (rotted) time++;
         }
-        
-        return fresh == 0 ? minutes : -1;
+    }
+
+    int orangesRotting(vector<vector<int>>& grid) {
+        int fresh = 0, time = 0;
+        int rows = grid.size(), cols = grid[0].size();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) fresh++;
+            }
+        }
+
+        vector<vector<bool>> vis(rows, vector<bool>(cols, false));
+        bfs(grid, vis, time, fresh);
+
+        return fresh == 0 ? time : -1;
     }
 };
