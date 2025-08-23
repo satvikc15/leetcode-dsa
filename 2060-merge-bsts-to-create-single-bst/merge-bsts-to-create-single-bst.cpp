6544bb908eda873ca100a35f78debb04f61a1f7e
Satvik Chindaluru <117159255+satvikc15@users.noreply.github.com>
2025-08-23 16:04:10 +0530

    Time: 559 ms (63.81%) | Memory: 431.1 MB (70.24%) - LeetSync

---
+++ 2060-merge-bsts-to-create-single-bst/merge-bsts-to-create-single-bst.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int,TreeNode*> roots;
+    unordered_set<int> child;
+    bool dfs(TreeNode* root,long low,long high){
+        if(root==NULL){
+            return true;
+        }
+        if (!(low < root->val && root->val < high)) return false;
+        
+           if(!root->left && !root->right){
+            if(roots.find(root->val)!=roots.end()){
+                root->left=roots[root->val]->left;
+                root->right=roots[root->val]->right;
+                roots.erase(root->val);  
+            }
+        }
+        return dfs(root->left, low, root->val) && dfs(root->right, root->val, high);
+    }
+    TreeNode* canMerge(vector<TreeNode*>& trees) {
+        for(auto it:trees){
+            roots[it->val]=it;
+            if(it->left) child.insert(it->left->val);
+            if(it->right) child.insert(it->right->val);
+        }
+        TreeNode* root=NULL;
+        for(auto it:trees){
+            if(child.find(it->val)==child.end()){   
+                if(root) return nullptr;  
+                root=it;
+            }
+        }
+        if(!root) return nullptr;
+        roots.erase(root->val);
+         if (!dfs(root, LONG_MIN, LONG_MAX)) return nullptr;
+         if (!roots.empty()) return nullptr;
+
+        return root;
+    }
+};
