29bfd866c672a8f7fa40453108ccd25a70837dce
Satvik Chindaluru <117159255+satvikc15@users.noreply.github.com>
2025-09-13 12:01:59 +0530

    Time: 3 ms (37.49%) | Memory: 9.8 MB (44.54%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.cpp
@@ -3,23 +3,20 @@ public:
     int maxFreqSum(string s) {
         vector<int> freq(26,0);
         map<char,int> mpp;
+        int maxVows = 0;
+        int maxCons=0;
         mpp['a']=0;mpp['e']=0;mpp['i']=0;mpp['o']=0;mpp['u']=0;
         for(auto it:s){
             if(mpp.find(it)!=mpp.end()){
                 mpp[it]++;
+                maxVows = max(maxVows,mpp[it]);
             }
             else{
                 freq[it-'a']++;
+                maxCons = max(maxCons,freq[it-'a']);
             }
         }
-        int maxVows = 0;
-        for(auto it:mpp){
-            maxVows = max(maxVows,it.second);
-        }
-        int maxCons=0;
-        for(auto it:freq){
-            maxCons = max(maxCons,it);
-        }
+    
         return maxCons+maxVows;
     }
 };
