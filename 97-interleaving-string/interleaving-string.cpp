210511e1b2cb2438edc6165d2c95c2a0c8630f70
Satvik Chindaluru <117159255+satvikc15@users.noreply.github.com>
2025-11-13 11:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (31.45%) - LeetSync

--- 97-interleaving-string/interleaving-string.cpp
+++ 97-interleaving-string/interleaving-string.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool fun(int i, int j, int k, string &s1, string &s2, string &s3, vector<vector<vector<int>>> &dp) {
+    bool fun(int i, int j, int k, string &s1, string &s2, string &s3, vector<vector<int>> &dp) {
         if (i == s1.size() && j == s2.size() && k == s3.size())
             return true;
 
         if (k == s3.size())
             return false;
 
-        if (dp[i][j][k] != -1)
-            return dp[i][j][k];
+        if (dp[i][j] != -1)
+            return dp[i][j];
 
         bool res = false;
 
@@ -18,16 +18,16 @@ public:
         if (j < s2.size() && s2[j] == s3[k])
             res |= fun(i, j + 1, k + 1, s1, s2, s3, dp);
 
-        return dp[i][j][k] = res;
+        return dp[i][j] = res;
     }
 
     bool isInterleave(string &s1, string &s2, string &s3) {
         if (s1.size() + s2.size() != s3.size())
             return false;
 
-        vector<vector<vector<int>>> dp(
+        vector<vector<int>>dp(
             s1.size() + 1,
-            vector<vector<int>>(s2.size() + 1, vector<int>(s3.size() + 1, -1))
+            vector<int>(s2.size() + 1, -1)
         );
 
         return fun(0, 0, 0, s1, s2, s3, dp);
