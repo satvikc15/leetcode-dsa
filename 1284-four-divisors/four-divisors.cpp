a0dd2b246ed61a418bab649af65bf3aba6538d24
Satvik Chindaluru <117159255+satvikc15@users.noreply.github.com>
2026-01-04 22:43:59 +0530

    Time: 1437 ms (17.24%) | Memory: 17.1 MB (26.85%) - LeetSync

--- 1284-four-divisors/four-divisors.cpp
+++ 1284-four-divisors/four-divisors.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    unordered_map<int,int> mpp;
     int pos(int n){
         int count=0;
         int temp=0;
@@ -14,12 +15,16 @@ public:
             }
             if(count>4) return 0;
         }
-        return count==4 ? temp:0;
+        return mpp[n]=count==4 ? temp:0;
     }
     int sumFourDivisors(vector<int>& nums) {
         int ans=0;
         for(auto it:nums){
+            if(mpp.find(it)==mpp.end()){
             ans+=pos(it);
+            }else{
+                ans+=mpp[it];
+            }
         }
         return ans;
     }
